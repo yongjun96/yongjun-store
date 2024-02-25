b68debbe0a0f6b9439993daa13c897696d6692bf
yongjun96 <practice960426@gmail.com>
2024-02-25 21:42:05 +0900

    yongjun-store 생성

---
+++ src/main/java/springboot/yongjunstore/YongjunStoreApplication.java
@@ -0,0 +1,13 @@
+package springboot.yongjunstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YongjunStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YongjunStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/yongjunstore/YongjunStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.yongjunstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YongjunStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
