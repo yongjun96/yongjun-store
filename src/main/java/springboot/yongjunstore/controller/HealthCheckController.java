19277698fa8608662eef9c523ffd11e68c03fc90
yongjun96 <practice960426@gmail.com>
2024-04-13 01:30:47 +0900

    로드 밸런스의 HealthCheck를 해줄 /health 생성

---
+++ src/main/java/springboot/yongjunstore/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package springboot.yongjunstore.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity findMember(){
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
