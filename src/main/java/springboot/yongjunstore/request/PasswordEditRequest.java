667f04a9ced7a4c741cfcb523729058d5cc8aaa0
yongjun96 <practice960426@gmail.com>
2024-04-24 15:05:36 +0900

    PasswordEditRequest Schema 추가

--- src/main/java/springboot/yongjunstore/request/PasswordEditRequest.java
+++ src/main/java/springboot/yongjunstore/request/PasswordEditRequest.java
@@ -1,5 +1,6 @@
 package springboot.yongjunstore.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -9,13 +10,16 @@ import lombok.Data;
 @Data
 public class PasswordEditRequest {
 
+    @Schema(description = "이메일", example = "practice960426@gmail.com")
     @Email(message = "이메일 형식이 알맞지 않습니다.")
     @NotBlank(message = "이메일은 필수값 입니다.")
     private String email;
 
+    @Schema(description = "변경할 비밀번호", example = "asdf!1234")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "최소 8자 이상, 하나 이상의 소문자, 숫자, 특수문자를 포함해야 합니다.")
     private String password;
 
+    @Schema(description = "변경할 비밀번호 확인", example = "asdf!1234")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "최소 8자 이상, 하나 이상의 소문자, 숫자, 특수문자를 포함해야 합니다.")
     private String passwordCheck;
 
